17148bc2674d136fd67e92f1b52cbef0aa43bb32
Ali Khajeh-Hosseini <alikhajeh1@googlemail.com>
2017-04-10 16:53:32 +0430

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,12 +10,12 @@ if [ ! -f /etc/phpmyadmin/config.user.inc.php ] ; then
   touch /etc/phpmyadmin/config.user.inc.php
 fi
 
-mkdir -p /var/nginx/client_body_temp
-chown nobody:nobody /sessions /var/nginx/client_body_temp
-mkdir -p /var/run/php/
-chown nobody:nobody /var/run/php/
+#mkdir -p /var/nginx/client_body_temp
+#chown nobody:nobody /sessions /var/nginx/client_body_temp
+#mkdir -p /var/run/php/
+#chown nobody:nobody /var/run/php/
 touch /var/log/php-fpm.log
-chown nobody:nobody /var/log/php-fpm.log
+#chown nobody:nobody /var/log/php-fpm.log
 
 if [ "$1" = 'phpmyadmin' ]; then
     exec supervisord --nodaemon --configuration="/etc/supervisord.conf" --loglevel=info
